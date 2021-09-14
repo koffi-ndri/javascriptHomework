30153ffe69ced9719ccf4275327d2c8d57ae8ebc
Andrew N'dri <koffi.ndri@amalitech.org>
2021-09-14 19:04:14 +0000

    Initial commit

---
+++ homework.js
@@ -0,0 +1,27 @@
+// Mary Rose's JavaScript Homework
+
+// 1. Write an if/else statement
+var APPLE_QUANTITY = 5;
+
+if (APPLE_QUANTITY < 5){ // if apple quantity is less than 5
+  console.log("You have fewer than 5 apples.");
+} else {
+  console.log("You have 5 or more apples. Hurrah!");
+}
+
+// 2. Write a "for" loop
+for(i = 0; i < APPLE_QUANTITY; i ++){
+  console.log("WEEEEEEE! I'm looping!") // The number of time this sentence will print depends on the quantity of apple
+}
+
+// 3. Write a function
+var sayHello = function(){
+  console.log("Hello!") //this function prints "Hello!"
+}
+
+// 4. Create an object
+var mary = {
+  name: "Mary Rose",
+  codeNinja: true,
+  country: "UK"
+}
